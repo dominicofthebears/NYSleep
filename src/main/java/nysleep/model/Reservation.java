ae46fbfbbb2cab6ed8a3e41b01da7d2ea4178000
dennymeo99 <meolivorno@gmail.com>
2022-12-28 12:15:10 +0100

    DTO added and review/reservation updated

--- src/main/java/nysleep/model/Reservation.java
+++ src/main/java/nysleep/model/Reservation.java
@@ -8,27 +8,21 @@ public class Reservation {
     private LocalDate dataEnd;
     private float totalCost;
     private int numPeople;
-    private long customerId;
-    private String customerFirstName;
-    private String customerLastName;
-    private long accommodationId;
-    private String accommodationName;
+    private Customer customer;
+    private Accommodation accommodation;
 
     public Reservation(){
 
     }
 
-    public Reservation(LocalDate dateRes, LocalDate dataStart, LocalDate dataEnd, float totalCost, int numPeople, long customerId, String customerFirstName, String customerLastName, long accommodationId, String accommodationName) {
+    public Reservation(LocalDate dateRes, LocalDate dataStart, LocalDate dataEnd, float totalCost, int numPeople, long customerId, String customerFirstName, String customerLastName, long accommodationId, String accommodationName, Customer customer, Accommodation accommodation) {
         this.dateRes = dateRes;
         this.dataStart = dataStart;
         this.dataEnd = dataEnd;
         this.totalCost = totalCost;
         this.numPeople = numPeople;
-        this.customerId = customerId;
-        this.customerFirstName = customerFirstName;
-        this.customerLastName = customerLastName;
-        this.accommodationId = accommodationId;
-        this.accommodationName = accommodationName;
+        this.customer = customer;
+        this.accommodation = accommodation;
     }
 
     public LocalDate getDateRes() {return dateRes;}
@@ -51,43 +45,19 @@ public class Reservation {
 
     public void setNumPeople(int numPeople) {this.numPeople = numPeople;}
 
-    public long getCustomerId() {
-        return customerId;
+    public Customer getCustomer() {
+        return customer;
     }
 
-    public void setCustomerId(long customerId) {
-        this.customerId = customerId;
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
     }
 
-    public String getCustomerFirstName() {
-        return customerFirstName;
+    public Accommodation getAccommodation() {
+        return accommodation;
     }
 
-    public void setCustomerFirstName(String customerFirstName) {
-        this.customerFirstName = customerFirstName;
-    }
-
-    public String getCustomerLastName() {
-        return customerLastName;
-    }
-
-    public void setCustomerLastName(String customerLastName) {
-        this.customerLastName = customerLastName;
-    }
-
-    public long getAccommodationId() {
-        return accommodationId;
-    }
-
-    public void setAccommodationId(long accommodationId) {
-        this.accommodationId = accommodationId;
-    }
-
-    public String getAccommodationName() {
-        return accommodationName;
-    }
-
-    public void setAccommodationName(String accommodationName) {
-        this.accommodationName = accommodationName;
+    public void setAccommodation(Accommodation accommodation) {
+        this.accommodation = accommodation;
     }
 }
