f7eb5f90737f5617df65a3dd466b56413b82c084
Salion0 <salvatore.patisso.00@gmail.com>
2023-01-09 12:44:00 +0100

    Merge pull request #4 from dominicofthebears/branch#3
    First DAO'S and model update

--- src/main/java/nysleep/DAO/base/MongoBaseDAO.java
+++ src/main/java/nysleep/DAO/base/MongoBaseDAO.java
@@ -15,7 +15,7 @@ import java.util.Iterator;
 
 public abstract class MongoBaseDAO{
     private static String connection = "mongodb://localhost:27017";
-    private static String dbName = "NYsleep";
+    private static String dbName = "NYSleep";
 
     public MongoBaseDAO(){}
 
